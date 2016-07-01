8aa805e66970b3c4db9590d763c88d98df28064e
Swati Joshi <jani.swati@gmail.com>
2016-06-30 22:21:18 -0700

    Data Visualization - Titanic exercise

---
+++ Titanic1.R
@@ -0,0 +1,18 @@
+titanic <- read.csv("~/Documents/Data Wrangling/DW_2/titanic_original.csv")
+
+# Check out the structure of titanic
+str(titanic)
+
+# Use ggplot() for the first instruction
+ggplot(titanic, aes(x=factor(pclass), fill=factor(sex))) + geom_bar(position="dodge")
+
+# Use ggplot() for the second instruction
+ggplot(titanic, aes(x=factor(pclass), fill=factor(sex))) + geom_bar(position="dodge") +
+  facet_grid(".~survived")
+
+# Position jitter (use below)
+posn.j <- position_jitter(0.5, 0)
+
+# Use ggplot() for the last instruction
+ggplot(titanic, aes(x=factor(pclass), y=age, col=factor(sex))) + 
+  geom_jitter(position=posn.j, size=3, alpha=0.5) + facet_grid(".~survived")
